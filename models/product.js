99f083e8693725662bc0b7b4d1d2b81b875373f1
andijayawizard <andijayawizard@gmail.com>
2024-03-01 16:40:41 +0700

    belajar orm dengan sequelize

---
+++ models/product.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Product.belongsTo(models.Category, {
+        foreignKey: 'categoryId',
+      });
+    }
+  }
+  Product.init({
+      name: DataTypes.STRING,
+      quantity: DataTypes.INTEGER,
+      inStock: DataTypes.BOOLEAN,
+      productImage: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      expiryDate: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
